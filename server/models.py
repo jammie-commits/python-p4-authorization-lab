99e4cee892f5267543db8f442970e76b093fe8c0
jay.mbugua.ph@gmail.com <jammie@DESKTOP-CDCBFL2.>
2024-07-23 23:49:26 +0300

    validations lab

--- server/models.py
+++ server/models.py
@@ -1,37 +1,91 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+db = SQLAlchemy()
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-class Article(db.Model, SerializerMixin):
-    __tablename__ = 'articles'
-
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String)
-    title = db.Column(db.String)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        
+        if not name:
+            raise ValueError("Name cannot be empty.")
+        
+        
+        if Author.query.filter(Author.name == name).first() is not None:
+            raise ValueError("Name must be unique.")
+
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+      
+        if not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError("Phone number must be exactly ten digits.")
+        
+        return phone_number
+        
+        
+    def __repr__(self):
+        return f'Author(id={self.id}, name={self.name})'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
-    preview = db.Column(db.String)
-    minutes_to_read = db.Column(db.Integer)
-    is_member_only = db.Column(db.Boolean, default=False)
-    date = db.Column(db.DateTime, server_default=db.func.now())
+    category = db.Column(db.String)
+    summary = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content.strip()) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return content
+    
+    @validates ('summary')
+    def validate_summary(self, key, summary):
+        if len(summary.strip()) > 250:
+            raise ValueError("Post summary must be less than 250 characters long.")
+        return summary
+    
+    @validates ('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Post category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
+        
+        if not any(word in title for word in clickbait_words):
+            raise ValueError("Post title must be sufficiently clickbait-y and contain one of: 'Won't Believe', 'Secret', 'Top', 'Guess'.")
+        
+        return title
+    
+    
+
 
     def __repr__(self):
-        return f'Article {self.id} by {self.author}'
-
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
-
-    articles = db.relationship('Article', backref='user')
-
-    def __repr__(self):
-        return f'User {self.username}, ID {self.id}'
+        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
